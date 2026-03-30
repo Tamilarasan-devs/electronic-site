04a3777001cf925d933a24e41884897ef21750e5
Tamilarasan-devs <tamilarasan.softwarengineer@gmail.com>
2026-03-30 13:42:44 +0530

    ui update

--- src/component/Banner.jsx
+++ src/component/Banner.jsx
@@ -6,14 +6,13 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 
-
 import bnr from "../assets/images/tv-repair.jpg";
 import bnr1 from "../assets/images/tv-repair-1.jpg";
 import bnr2 from "../assets/images/tv-repair-2.jpg";
 import bnr3 from "../assets/images/tv-repair-3.jpg";
 
 export default function Banner() {
-  const images = [bnr, bnr1, bnr3,bnr2];
+  const images = [bnr, bnr1, bnr3, bnr2];
 
   return (
     <div className="w-full">
@@ -28,14 +27,14 @@ export default function Banner() {
         }}
         pagination={{ clickable: true }}
         navigation={true}
-        className="w-full h-[800px]"
+        className="w-full h-[250px] sm:h-[350px] md:h-[500px] lg:h-[650px]"
       >
         {images.map((img, i) => (
           <SwiperSlide key={i}>
             <img
               src={img}
-              alt=""
-              className="w-full h-[800px] "
+              alt={`slide-${i}`}
+              className="w-full h-full object-cover"
             />
           </SwiperSlide>
         ))}
