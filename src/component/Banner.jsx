2a7d12aadb04d9e295d29223118a32889e3d1716
Tamilarasan-devs <tamilarasan.softwarengineer@gmail.com>
2026-04-07 15:20:21 +0530

    ui

--- src/component/Banner.jsx
+++ src/component/Banner.jsx
@@ -17,10 +17,10 @@ import bnr7 from "../assets/images/bner/7.jpg";
 import bnr8 from "../assets/images/bner/8.jpg";
 
 export default function Banner() {
-  const images = [bnr, bnr1, bnr3, bnr2,bnr4, bnr5, bnr6, bnr7, bnr8];
+  const images = [bnr, bnr1, bnr3, bnr2, bnr4, bnr5, bnr6, bnr7, bnr8];
 
   return (
-    <div className="w-full p-6">
+    <div className="w-full">
       <Swiper
         modules={[Autoplay, Pagination, Navigation]}
         spaceBetween={0}
@@ -32,15 +32,17 @@ export default function Banner() {
         }}
         pagination={{ clickable: true }}
         navigation={true}
-        className="w-full h-[250px] sm:h-[350px] md:h-[500px] lg:h-[650px]"
+        className="w-full"
       >
         {images.map((img, i) => (
           <SwiperSlide key={i}>
-            <img
-              src={img}
-              alt={`slide-${i}`}
-              className="w-full h-full object-cover"
-            />
+            <div className="w-full h-[200px] sm:h-[300px] md:h-[450px] lg:h-[600px] xl:h-[700px]">
+              <img
+                src={img}
+                alt={`slide-${i}`}
+                className="w-full h-full object-cover"
+              />
+            </div>
           </SwiperSlide>
         ))}
       </Swiper>
